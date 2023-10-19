8e30ee53f6806512e0cd687532f03f781c4b30a7
Jonathan Robles <jonathan.robles@accenturefederal.com>
2023-10-19 17:28:29 -0500

    Added to program.cs

--- ChristinasPieShop/Program.cs
+++ ChristinasPieShop/Program.cs
@@ -1,4 +1,9 @@
+using ChristinasPieShop.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+builder.Services.AddScoped<IPieRepository,  MockPieRepository>();
 
 builder.Services.AddControllersWithViews();
 
