802e5871c0bd7cd3afa3dcee2e61e0282481eea0
Jonathan Robles <91964848+robles-jonathan@users.noreply.github.com>
2023-10-20 17:25:24 -0500

    Created PieRepository and CategoryRepository

---
+++ ChristinasPieShop/Models/CategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace ChristinasPieShop.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ChristinasPieShopDbContext _christinasPieShopDbContext;
+        public CategoryRepository(ChristinasPieShopDbContext christinasPieShopDbContext)
+        {
+            _christinasPieShopDbContext = christinasPieShopDbContext;
+        }
+
+        public IEnumerable<Category> AllCategories => _christinasPieShopDbContext.Categories.OrderBy(p => p.CategoryName);
+    }
+}
---
+++ ChristinasPieShop/Models/PieRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ChristinasPieShop.Models
+{
+    public class PieRepository : IPieRepository
+    {
+        private readonly ChristinasPieShopDbContext _christinasPieShopDbContext;
+
+        public PieRepository(ChristinasPieShopDbContext christinasPieShopContext)
+        {
+            _christinasPieShopDbContext = christinasPieShopContext;
+        }
+
+        public IEnumerable<Pie> AllPies
+        {
+            get
+            {
+                return _christinasPieShopDbContext.Pies.Include(c => c.Category);
+            }
+        }
+
+        public IEnumerable<Pie> PiesOfTheWeek
+        {
+            get
+            {
+                return _christinasPieShopDbContext.Pies.Include(c => c.Category).Where(p => p.IsPieOfTheWeek);
+            }
+        }
+
+        public Pie? GetPieById(int pieId)
+        {
+            return _christinasPieShopDbContext.Pies.FirstOrDefault(p => p.PieId == pieId);
+        }
+    }
+}
